9f1c0597c7814a9b792f3cd66af286916e25dffc
Тарас <Acroyd1984@gmail.com>
2023-07-20 14:28:00 +0300

    Start

---
+++ src/main/java/com/example/homework2_11calculator/Homework211CalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.homework2_11calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework211CalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework211CalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homework2_11calculator/Homework211CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework2_11calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework211CalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
